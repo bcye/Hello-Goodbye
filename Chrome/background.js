e2d502f6bf494f891551a43746aab0b45696d134
Bruce <t15thbruce@outlook.de>
2019-03-02 10:33:25 -0800

    fix bugs, create promotional screenshot

--- Chrome/background.js
+++ Chrome/background.js
@@ -3,7 +3,7 @@
     chrome.webRequest.onBeforeRequest.addListener(
             function(details) { return {cancel: true}; },
             {urls: [
-              "*://widget.intercom.io/*"",
+              "*://widget.intercom.io/*",
               "*://connect.facebook.net/en_US/sdk/xfbml.customerchat.js",
               "*://assets.producthunt.com/assets/upwigloader.js"
             ]},
