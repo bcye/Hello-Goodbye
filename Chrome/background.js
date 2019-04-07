b2a0763fadad62102fb7949b130eff4401fcf521
Bruce <t15thbruce@outlook.de>
2019-04-07 13:07:39 -0700

    block zendesk (again)

--- Chrome/background.js
+++ Chrome/background.js
@@ -46,6 +46,7 @@ chrome.webRequest.onBeforeRequest.addListener(
                 "*://*.justanswer.com/revizely/2/core/ja-com.js",
                 "*://code.snapengage.com/*",
                 "*://sdk.inbenta.io/chatbot/*",
-                "*://pi2.movoto.com/1.7.654/javascripts/desktop/movoto.min.js"
+                "*://pi2.movoto.com/1.7.654/javascripts/desktop/movoto.min.js",
+                "*://*.zopim.com/*"
               ]},
               ["blocking"]);
