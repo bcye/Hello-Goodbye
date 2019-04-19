82dea2d1b5051bf9eb8bd2c67cb0482c5aa5d432
Bruce <t15thbruce@outlook.de>
2019-04-18 19:41:39 -0700

    more apple

---
+++ HelloGoodbyeiOS/Pods/Target Support Files/Pods-BlockerExtension/Pods-BlockerExtension-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BlockerExtension : NSObject
+@end
+@implementation PodsDummy_Pods_BlockerExtension
+@end
---
+++ HelloGoodbyeiOS/Pods/Target Support Files/Pods-HelloGoodbyeiOS/Pods-HelloGoodbyeiOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HelloGoodbyeiOS : NSObject
+@end
+@implementation PodsDummy_Pods_HelloGoodbyeiOS
+@end
