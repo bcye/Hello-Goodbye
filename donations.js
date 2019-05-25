87256bc23f1e99345bf33c79f0f23699cab00025
Bruce <t15thbruce@outlook.de>
2019-05-24 18:08:37 -0700

    Change to checkout and live version

--- donations.js
+++ donations.js
@@ -24,61 +24,80 @@ window.onclick = function(event) {
   }
 }
 
-// Stripe Elements Checkout
-var stripe = Stripe('pk_test_WTJEuHTeeBXA8P5FMFnUyRKo');
-var elements = stripe.elements();
+// Donations Stripe
+var stripe = Stripe('pk_live_e1426vA2ut4JcUtvhqde23Mu00WNjsII3R ');
 
-// Custom styling can be passed to options when creating an Element.
-var style = {
-  base: {
-    // Add your base input styles here. For example:
-    fontSize: '16px',
-    color: "#32325d",
-  }
-};
+var checkoutButton1 = document.getElementById('checkout-button-sku_F7jauZru39kVa7');
+checkoutButton1.addEventListener('click', function () {
+  // When the customer clicks on the button, redirect
+  // them to Checkout.
+  stripe.redirectToCheckout({
+    items: [{sku: 'sku_F7jauZru39kVa7', quantity: 1}],
 
-// Create an instance of the card Element.
-var card = elements.create('card', {style: style});
-
-// Add an instance of the card Element into the `card-element` <div>.
-card.mount('#card-element');
-
-card.addEventListener('change', function(event) {
-  var displayError = document.getElementById('card-errors');
-  if (event.error) {
-    displayError.textContent = event.error.message;
-  } else {
-    displayError.textContent = '';
-  }
+    // Do not rely on the redirect to the successUrl for fulfilling
+    // purchases, customers may not always reach the success_url after
+    // a successful payment.
+    // Instead use one of the strategies described in
+    // https://stripe.com/docs/payments/checkout/fulfillment
+    successUrl: 'https://hellogoodbye.app/download',
+    cancelUrl: 'https://hellogoodbye.app/canceled',
+  })
+    .then(function (result) {
+      if (result.error) {
+        // If `redirectToCheckout` fails due to a browser or network
+        // error, display the localized error message to your customer.
+        var displayError = document.getElementById('error-message');
+        displayError.textContent = result.error.message;
+      }
+    });
 });
 
-// Create a token or display an error when the form is submitted.
-var form = document.getElementById('payment-form');
-form.addEventListener('submit', function(event) {
-  event.preventDefault();
+var checkoutButton3 = document.getElementById('checkout-button-sku_F7jcR9UoUyQLuH');
+checkoutButton3.addEventListener('click', function () {
+  // When the customer clicks on the button, redirect
+  // them to Checkout.
+  stripe.redirectToCheckout({
+    items: [{sku: 'sku_F7jcR9UoUyQLuH', quantity: 1}],
 
-  stripe.createToken(card).then(function(result) {
-    if (result.error) {
-      // Inform the customer that there was an error.
-      var errorElement = document.getElementById('card-errors');
-      errorElement.textContent = result.error.message;
-    } else {
-      // Send the token to your server.
-      stripeTokenHandler(result.token);
-      window.location.href = "https://hellogoodbye.app/download";
-    }
-  });
+    // Do not rely on the redirect to the successUrl for fulfilling
+    // purchases, customers may not always reach the success_url after
+    // a successful payment.
+    // Instead use one of the strategies described in
+    // https://stripe.com/docs/payments/checkout/fulfillment
+    successUrl: 'https://hellogoodbye.app/download',
+    cancelUrl: 'https://hellogoodbye.app/canceled',
+  })
+    .then(function (result) {
+      if (result.error) {
+        // If `redirectToCheckout` fails due to a browser or network
+        // error, display the localized error message to your customer.
+        var displayError = document.getElementById('error-message');
+        displayError.textContent = result.error.message;
+      }
+    });
 });
 
-function stripeTokenHandler(token) {
-  // Insert the token ID into the form so it gets submitted to the server
-  var form = document.getElementById('payment-form');
-  var hiddenInput = document.createElement('input');
-  hiddenInput.setAttribute('type', 'hidden');
-  hiddenInput.setAttribute('name', 'stripeToken');
-  hiddenInput.setAttribute('value', token.id);
-  form.appendChild(hiddenInput);
+var checkoutButton5 = document.getElementById('checkout-button-sku_F83UXxToMdMbWN');
+checkoutButton5.addEventListener('click', function () {
+  // When the customer clicks on the button, redirect
+  // them to Checkout.
+  stripe.redirectToCheckout({
+    items: [{sku: 'sku_F83UXxToMdMbWN', quantity: 1}],
 
-  // Submit the form
-  form.submit();
-}
+    // Do not rely on the redirect to the successUrl for fulfilling
+    // purchases, customers may not always reach the success_url after
+    // a successful payment.
+    // Instead use one of the strategies described in
+    // https://stripe.com/docs/payments/checkout/fulfillment
+    successUrl: 'https://hellogoodbye.app/download',
+    cancelUrl: 'https://hellogoodbye.app/canceled',
+  })
+    .then(function (result) {
+      if (result.error) {
+        // If `redirectToCheckout` fails due to a browser or network
+        // error, display the localized error message to your customer.
+        var displayError = document.getElementById('error-message');
+        displayError.textContent = result.error.message;
+      }
+    });
+});
--- functions/charge.js
+++
@@ -1,3 +0,0 @@
-exports.handler = function(event, context, callback) {
-
-};
