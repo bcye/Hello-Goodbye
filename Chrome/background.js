2948fb70059b103fa42575418a2ebd76d6a8a116
bcye <br.apps@icloud.com>
2019-08-17 11:19:53 +0200

    remove paywall

--- Chrome/background.js
+++ Chrome/background.js
@@ -1,21 +1,7 @@
 var isDisabled = false;
-var verified = false;
-
-chrome.runtime.onMessageExternal.addListener(function (message) {
-  console.log('message received');
-  if (message.verified) {
-    console.log('verified');
-    chrome.storage.sync.set({ 'verified': message.verified })
-    // reloads extension so that changes get applied.
-    }
-
-  return true;
-});
-chrome.runtime.sendMessage({ greeting: 'this is just a weird test lol' })
 
 // extension can be disabled temporarily
 chrome.storage.onChanged.addListener(function(changes, namespace) {
-  console.log('hello from a listeeenerrrr.')
   for (var key in changes) {
     var storageChange = changes[key].newValue;
     if (key === 'disabled') {
@@ -23,62 +9,69 @@ chrome.storage.onChanged.addListener(function(changes, namespace) {
       isDisabled = storageChange;
       console.log(isDisabled);
     }
-
-    if (key === 'verified') {
-      if (storageChange) {
-        chrome.webRequest.onBeforeRequest.addListener(
-                      function(details) {
-                        // set badge text to indicate that a help widget is available
-                        chrome.browserAction.setBadgeText({text: "HELP", tabId: details.tabId});
-
-                          if (isDisabled) {
-                            return { cancel: false } // this should return from the function (details) level
-                          } else {
-                            return { cancel: true }
-                          }
-                      },
-                      {urls: [
-                        "*://widget.intercom.io/*",
-                        "*://connect.facebook.net/en_US/sdk/xfbml.customerchat.js",
-                        "*://assets.producthunt.com/assets/upwigloader.js",
-                        "*://js.driftt.com/include/*",
-                        "*://*.crisp.chat/*",
-                        "*://*.intergram.xyz/js/*",
-                        "*://widget.mfy.im/*",
-                        "*://connect.podium.com/*",
-                        "*://js.usemessages.com/*",
-                        "*://static.getchipbot.com/",
-                        "*://static.zdassets.com/ekr/snippet.js",
-                        "*://www.couchbase.com/webfiles/1552355627964/js/contact-popup-form.js",
-                        "*://assetscdn-wchat.freshchat.com/*",
-                        "*://wchat.freshchat.com/*",
-                        "*://code.jivosite.com/script/widget/*",
-                        "*://code.tidio.co/*",
-                        "*://*.user.com/static/js/*",
-                        "*://secure.livechatinc.com/*",
-                        "*://*.justanswer.com/revizely/2/core/ja-com.js",
-                        "*://code.snapengage.com/*",
-                        "*://sdk.inbenta.io/chatbot/*",
-                        "*://pi2.movoto.com/1.7.654/javascripts/desktop/movoto.min.js",
-                        "*://*.zopim.com/*",
-                        "*://cdn.gubagoo.io/*",
-                        "*://leadconnect.ipmaxi.se/*",
-                        "*://static.small.chat/messenger.js",
-                        "*://smartsupp-widget-161959.c.cdn77.org/build/smartchat-2.3.20.min.js",
-                        "*://plugins.help.com/*",
-
-                        // block consent banners
-                        "*://consent-manager.metomic.io/embed.js",
-                        "*://cdn.jsdelivr.net/npm/cookie-bar/*",
-                        "*://*.PrivacyPolicies.com/cookie-consent/*",
-                        "*://cdn.jsdelivr.net/npm/cookieconsent@3/*",
-                        "*://cdn.ziffstatic.com/jst/eu/zdconsent.js"
-                      ]},
-                      ["blocking"]);
-                     }
-      }
     }
   });
+
+chrome.webRequest.onBeforeRequest.addListener(
+  function(details) {
+    // set badge text to indicate that a help widget is available
+    chrome.browserAction.setBadgeText({text: "HELP", tabId: details.tabId});
+
+    if (isDisabled) {
+      return { cancel: false } // this should return from the function (details) level
+    } else {
+      return { cancel: true }
+    }
+  },
+  {urls: [
+    "*://widget.intercom.io/*",
+    "*://connect.facebook.net/en_US/sdk/xfbml.customerchat.js",
+    "*://assets.producthunt.com/assets/upwigloader.js",
+    "*://js.driftt.com/include/*",
+    "*://*.crisp.chat/*",
+    "*://*.intergram.xyz/js/*",
+    "*://widget.mfy.im/*",
+    "*://connect.podium.com/*",
+    "*://js.usemessages.com/*",
+    "*://static.getchipbot.com/",
+    "*://static.zdassets.com/ekr/snippet.js",
+    "*://www.couchbase.com/webfiles/1552355627964/js/contact-popup-form.js",
+    "*://assetscdn-wchat.freshchat.com/*",
+    "*://wchat.freshchat.com/*",
+    "*://code.jivosite.com/script/widget/*",
+    "*://code.tidio.co/*",
+    "*://*.user.com/static/js/*",
+    "*://secure.livechatinc.com/*",
+    "*://*.justanswer.com/revizely/2/core/ja-com.js",
+    "*://code.snapengage.com/*",
+    "*://sdk.inbenta.io/chatbot/*",
+    "*://pi2.movoto.com/1.7.654/javascripts/desktop/movoto.min.js",
+    "*://*.zopim.com/*",
+    "*://cdn.gubagoo.io/*",
+    "*://leadconnect.ipmaxi.se/*",
+    "*://static.small.chat/messenger.js",
+    "*://smartsupp-widget-161959.c.cdn77.org/build/smartchat-2.3.20.min.js",
+    "*://plugins.help.com/*",
+    "*://js.gs-chat.com/*",
+    "*://widget.customerly.io/*",
+
+
+    // block consent banners
+    "*://consent-manager.metomic.io/embed.js",
+    "*://cdn.jsdelivr.net/npm/cookie-bar/*",
+    "*://*.PrivacyPolicies.com/cookie-consent/*",
+    "*://cdn.jsdelivr.net/npm/cookieconsent@3/*",
+    "*://cdn.ziffstatic.com/jst/*/zdconsent.js",
+    "*://guce.yahoo.com/*",
+    "*://*/js/xf/notice.min.js?*",
+    "*://*/wp/wp-content/themes/v3ct/lib/cookies.min.js",
+    "*://*/wp/wp-content/plugins/woocommerce/assets/js/js-cookie/js.cookie.min.js?*",
+    "*://consent.truste.com/*",
+    "*://*.quantserve.com/*",
+    "*://*.consensu.org/*",
+    "*://cdn.componentator.com/spa.min*",
+  ]},
+  ["blocking"]);
 /*
               chrome.runtime.onInstalled.addListener(function() {
 
@@ -90,4 +83,3 @@ chrome.storage.onChanged.addListener(function(changes, namespace) {
                 return false;
               });
 */
-console.log("hello from the background");
