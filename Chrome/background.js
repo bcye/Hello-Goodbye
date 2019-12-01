6bc9054cef68c1c42341fce9bff3044cd803a891
bcye <br.apps@icloud.com>
2019-12-01 17:59:50 +0100

    block carnow.com

--- Chrome/background.js
+++ Chrome/background.js
@@ -61,7 +61,7 @@ chrome.webRequest.onBeforeRequest.addListener(
       "*://*.contactatonce.com/*",
       "*://live-chat-cdn.socialsignin.net/*",
       "*://messenger.ngageics.com/*",
-      "*://app.carnow.com/dealers/carnow_plugin.js?*"
+      "*://app.carnow.com/dealers/carnow_plugin.js?*",
 
       // someone from x just bought y widgets
       "*://static.notifia.io/widget.js",
